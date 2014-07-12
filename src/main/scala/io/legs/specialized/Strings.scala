821b4c74e61589bd6ee4d2d99cb2667da31323d1
Roman Land <roman.land@gmail.com>
2014-07-12 17:02:38 +0300

    att string trim support

--- src/main/scala/io/legs/specialized/Strings.scala
+++ src/main/scala/io/legs/specialized/Strings.scala
@@ -8,7 +8,7 @@ import scala.concurrent._
 object Strings extends Specialization {
 
 	def EXTRACT_REGEX(state: Specialization.State, input:String, regex:String)(implicit ctx : ExecutionContext) : RoutableFuture =
-		future {
+		Future {
 			val r = regex.r
 			input match {
 				case r(output) => 	Success(Yield(Some(output)))
@@ -17,19 +17,24 @@ object Strings extends Specialization {
 		}
 
 	def REPLACE_REGEX(state:Specialization.State, input:String, matchStr:String, replaceStr:String)(implicit ctx : ExecutionContext) : RoutableFuture =
-		future {
+		Future {
 			val result = input.replaceAll(matchStr, replaceStr)
 			Success(Yield(Some(result)))
 		}
 
 	def SPLIT(state:Specialization.State, input:String, splitBy:String)(implicit ctx : ExecutionContext) : RoutableFuture =
-		future {
+		Future {
 			if (splitBy.length == 1){
 				val result = input.split(splitBy.toCharArray.head).toList
 				Success(Yield(Some(result)))
 			} else {
 				Failure(new Throwable("Strings/SPLIT currenly supports splitting by string of length 1"))
 			}
+		}
+
+	def TRIM(state:Specialization.State, input:String)(implicit ctx : ExecutionContext) : RoutableFuture =
+		Future {
+			Success(Yield(Some(input.trim())))
 		}
 
 }
